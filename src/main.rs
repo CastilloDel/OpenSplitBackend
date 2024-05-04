bf3e43c359e8d238030fd2f8aa3e867a39ae4ad5
CastilloDel <delcastillodelarosadaniel@gmail.com>
2024-05-04 16:24:39 +0200

    Add endpoint for adding expenses

--- src/main.rs
+++ src/main.rs
@@ -40,6 +40,27 @@ async fn get_balance(client: web::Data<Client>, id: web::Path<String>) -> HttpRe
     }
 }
 
+#[post("/groups/{id}/expenses")]
+async fn add_expense(
+    client: web::Data<Client>,
+    id: web::Path<String>,
+    expense: web::Json<Expense>,
+) -> HttpResponse {
+    let groups = client.database("OpenSplit").collection::<Group>("Groups");
+    let id = id.into_inner();
+    match groups
+        .update_one(
+            doc! { "id": id},
+            doc! { "$push": { "expenses": bson::to_bson(&expense.into_inner()).unwrap()}},
+            None,
+        )
+        .await
+    {
+        Ok(_) => HttpResponse::Ok().body("Expense added"),
+        Err(err) => return HttpResponse::InternalServerError().body(err.to_string()),
+    }
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let uri = std::env::var("MONGODB_URI").expect("You need to add the MONGODB_URI to the env");
@@ -53,6 +74,7 @@ async fn main() -> std::io::Result<()> {
             .app_data(web::Data::new(client.clone()))
             .service(add_group)
             .service(get_balance)
+            .service(add_expense)
     })
     .bind(("0.0.0.0", 8080))?
     .run()
